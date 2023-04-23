f61a9eb51eb1d8d5b00da3d15cf13bf36e361762
Hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2023-04-23 14:08:09 +0100

    base folders structures and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './private/dashboard/dashboard.module';
+import { ListProductComponent } from './private/list-product/list-product.component';
+import { ListProductModule } from './private/list-product/list-product.module';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
+import { AllProductComponent } from './public/components/all-product/all-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductComponent,
+    AllProductComponent,
+    PublicComponent,
+    PublicComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    ListProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {};
+
+const BASE_URL = "http://localhost:3000";
